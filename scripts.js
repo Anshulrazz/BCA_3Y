a7cef92d83a588d3e5b46a5dc9ff970599d0a6c7
Anshulrazz <braj70901@gmail.com>
2024-08-12 11:48:26 +0530

    as

--- scripts.js
+++ scripts.js
@@ -1,19 +1,37 @@
-let fname ='ANSHUL';
+// let fname ='ANSHUL';
 
-document.write("Hello, &nbsp;", fname,"<br>");
+// document.write("Hello, &nbsp;", fname,"<br>");
 
-let age = 21;
+// let age = 21;
 
-document.write("Age: ", age,"<br>");
+// document.write("Age: ", age,"<br>");
 
-let isStudent = true;
+// let isStudent = true;
 
-document.write(`Student: ${isStudent ? "Yes" : "No"}`,"<br>");
+// document.write(`Student: ${isStudent ? "Yes" : "No"}`,"<br>");
 
-let emptyValue = null;
+// let emptyValue = null;
 
-document.write(`About Us: ${emptyValue || "Write Something"}`,"<br>");
+// document.write(`About Us: ${emptyValue || "Write Something"}`,"<br>");
 
-let uninitialised;
+// let uninitialised;
 
-document.write(!uninitialised && "Fill complete details","<br>");
+// document.write(!uninitialised && "Fill complete details","<br>");
+
+
+
+
+
+// let age = 21;
+// document.write(age > 17 ?  "You can vote" : "You can't vote" );
+
+
+// let number = prompt("Enter a number");
+
+// if (number < 0) {
+//     document.write("The number is negative");
+// } else if (number > 0) {
+//     document.write("The number is positive");
+// } else {
+//     document.write("The number is zero");
+// }
