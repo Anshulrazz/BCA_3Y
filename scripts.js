fcada1cfbf3a8367b02ff04e7fc7d9ce3956769a
Anshulrazz <braj70901@gmail.com>
2024-08-13 15:30:14 +0530

    calculator

--- scripts.js
+++ scripts.js
@@ -37,18 +37,53 @@
 // }
 
 
-let fruit = prompt("Enter a fruit");
+// let fruit = prompt("Enter a fruit");
 
-switch (fruit) {
-    case "apple":
-        document.write(`You choose : ${fruit}`);
-        break;
-    case "banana":
-        document.write(`You choose : ${fruit}`);
-        break;
-    default:
-        document.write(`You choose unkonwn `);
+// switch (fruit) {
+//     case "apple":
+//         document.write(`You choose : ${fruit}`);
+//         break;
+//     case "banana":
+//         document.write(`You choose : ${fruit}`);
+//         break;
+//     default:
+//         document.write(`You choose unkonwn `);
+// }
+
+let greet = () => {
+    let date = new Date();
+    if (date.getHours() < 12) {
+        document.getElementById("demo").innerHTML = "Good Morning";
+    } else if (date.getHours() < 18) {
+        document.getElementById("demo").innerHTML = ("Good Afternoon");
+    } else {
+        document.getElementById("demo").innerHTML = ("Good Evening");
+    }
 }
 
+let themeDay = () => {
+document.body.style.backgroundColor = "#ccc";
+document.body.style.color = "black";
 
+}
+let themeNight = () => {
+document.body.style.backgroundColor = "#1E201E";
+document.body.style.color = "white";
+}
 
+function updateClock() {
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+updateClock();
+setInterval(updateClock, 1000);
